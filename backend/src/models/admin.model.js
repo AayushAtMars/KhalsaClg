7767d76426151694d084764e15baf0bc3a3c54ac
AayushAtMars <yoaayush14@gmail.com>
2025-12-18 18:28:48 +0530

    2nd

--- backend/src/models/admin.model.js
+++ backend/src/models/admin.model.js
@@ -4,8 +4,7 @@ const adminSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, default: "admin" },
-  isSuperAdmin: { type: Boolean, default: false }
+  role: { type: String, default: "admin" }
 }, { timestamps: true });
 
 export default mongoose.model("Admin", adminSchema);
